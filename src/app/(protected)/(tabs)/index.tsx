a36e29e1657f82c341ac78c29d2802212142d6e7
Khan Abdul Rehman <arktechsolution7@gmail.com>
2025-07-12 18:23:36 +0530

    12 July 2025 -By Abdul Rehman - (New Post UI)

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 import { dummyPosts } from "@/dummyData";
-import { Text } from "react-native";
 import { FlatList } from "react-native";
 import PostListItem from "@/components/PostListItem";
+import { Link } from "expo-router";
 
 export default function Home() {
   return (
     <FlatList
       data={dummyPosts}
       renderItem={({ item }) => <PostListItem post={item} />}
+      ListHeaderComponent={() => (
+        <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
+          New Post
+        </Link>
+      )}
     />
   );
 }
---
+++ src/app/(protected)/new.tsx
@@ -0,0 +1,43 @@
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+
+export default function NewPost() {
+  const [text, setText] = useState("");
+  return (
+    <SafeAreaView edges={["bottom"]} className="p-4 flex-1">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 140 : 0}
+        className="flex-1"
+      >
+        <Text className="text-white text-lg font-bold">username</Text>
+
+        <TextInput
+          value={text}
+          placeholder="What is on your mind?"
+          onChangeText={setText}
+          className="text-white text-lg"
+          multiline
+          numberOfLines={4}
+        />
+
+        <View className="mt-auto">
+          <Pressable
+            onPress={() => console.log("post: ", text)}
+            className="bg-white p-3 px-6 self-end rounded-full"
+          >
+            <Text className="text-black font-bold">Post</Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
